69828e8adeac7df4113a01e94beb8457ef8daadb
Abdul Moiz Hussain <33151673+abdulmoizhussain@users.noreply.github.com>
2019-08-07 12:48:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+
+// adb devices
+// adb -s <device_name> tcp:<SourcePort> tcp:<DestinationPort>
+// -----------------------------------------------
 // Source: https://stackoverflow.com/a/26701517
 const http = require("http"), httpProxy = require("http-proxy");
 const
   PROTOCOL = "http",
-  // HOST_NAME = "192.168.0.101",
-  HOST_NAME = "localhost",
-  PORT = 5050;
+  HOST_NAME = "192.168.0.101",
+  // HOST_NAME = "localhost",
+  PORT = 8677;
 
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: `${PROTOCOL}://${HOST_NAME}:${PORT}` });
+  const uri = `${PROTOCOL}://${HOST_NAME}:${PORT}`;
+  proxy.web(req, res, { target: uri });
+  console.log(`${req.method}: http://localhost:${PORT + req.url} --> ${uri}/....`);
 });
 
 server.listen(PORT);
-console.log("LISTENING ON PORT: " + PORT);
+console.log("LISTENING: http://localhost:" + PORT);
